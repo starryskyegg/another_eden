8b7a04d90809d508468ea3cc9e7f12923f9ef31a
starryskyegg <78290099+starryskyegg@users.noreply.github.com>
2021-02-02 12:21:33 +0800

    Add files via upload

--- 物防測量器.py
+++ 物防測量器.py
@@ -6,124 +6,42 @@ mode = int(input())
 DEF = 0
 d1 = []
 d2 = []
-d3 = []
 
+print('請輸入攻擊數值')
+ATK = int(input())
+print('請輸入腕力數值')
+PWR = int(input())
 if mode == 1:
-    print('請輸入攻擊數值')
-    ATK = int(input())
-    print('請輸入腕力數值')
-    PWR = int(input())
-
-    print('請輸入傷害值')
-    rDMG = int(input())
-    for i in range(16,48):
-        while DEF <= ATK*2:
-            tDMG = math.floor((ATK-DEF/2)*(PWR/32+1)*1.75+ATK*i/25.6)
-            if tDMG == rDMG:
-                d1.append(DEF)
-            DEF+=1
-        DEF = 0
-
-    k = len(d1)
-    while k != 1:
-        j = 0
-        p = 0
-        print('請輸入傷害值')
-        rDMG = int(input())
-        for i in range(16,48):
-            while DEF <= ATK*2:
-                tDMG = math.floor((ATK-DEF/2)*(PWR/32+1)*1.75+ATK*i/25.6)
-                if tDMG == rDMG:
-                    d2.append(DEF)
-                DEF+=1
-            DEF = 0
-        while j < len(d1):
-            if d1[j] in d2 :
-                d3.append(d1[j])
-            j+=1
-        d1 = d3
-        d2 = []
-        d3 = []
-        k = len(d1)
-    print('敵方的物防是'+str(d1[0]))
+    RST = 1
 elif mode == 2:
-    print('請輸入攻擊數值')
-    ATK = int(input())
     print('請輸入魔力數值')
     MATK = int(input())
-    print('請輸入腕力數值')
-    PWR = int(input())
-
-    print('請輸入傷害值')
-    rDMG = int(input())
-    for i in range(16,48):
-        while DEF <= ATK*2:
-            nBASE = (ATK-DEF/2)*(PWR/32+1)*1.75
-            tDMG = math.floor(math.floor(nBASE+ATK*i/25.6)*((MATK+(2*MATK)**0.5)/512+1.85))
-            if tDMG == rDMG:
-                d1.append(DEF)
-            DEF+=1
-        DEF = 0
-
-    k = len(d1)
-    while k != 1:
-        j = 0
-        p = 0
-        print('請輸入傷害值')
-        rDMG = int(input())
-        for i in range(16,48):
-            while DEF <= ATK*2:
-                nBASE = (ATK-DEF/2)*(PWR/32+1)*1.75
-                tDMG = math.floor(math.floor(nBASE+ATK*i/25.6)*((MATK+(2*MATK)**0.5)/512+1.85))
-                if tDMG == rDMG:
-                    d2.append(DEF)
-                DEF+=1
-            DEF = 0
-        while j < len(d1):
-            if d1[j] in d2 :
-                d3.append(d1[j])
-            j+=1
-        d1 = d3
-        d2 = []
-        d3 = []
-        k = len(d1)
-    print('敵方的物防是'+str(d1[0]))
+    RST = (MATK+(2*MATK)**0.5)/512+1.85
 elif mode == 3:
-    print('請輸入攻擊數值')
-    ATK = int(input())
-    print('請輸入腕力數值')
-    PWR = int(input())
+    RST = 0.25
 
+print('請輸入傷害值')
+rDMG = int(input())
+for i in range(16,48):
+    while DEF <= ATK*2:
+        tDMG = math.floor(math.floor((ATK-DEF/2)*(PWR/32+1)*1.75+ATK*i/25.6)*RST)
+        if tDMG == rDMG:
+            d1.append(DEF)
+        DEF+=1
+    DEF = 0
+
+k = len(d1)
+while k != 1:
+    j = 0
     print('請輸入傷害值')
     rDMG = int(input())
     for i in range(16,48):
-        while DEF <= ATK*2:
-            tDMG = math.floor(math.floor((ATK-DEF/2)*(PWR/32+1)*1.75+ATK*i/25.6)*0.25)
+        for DEF in d1:
+            tDMG = math.floor(math.floor((ATK-DEF/2)*(PWR/32+1)*1.75+ATK*i/25.6)*RST)
             if tDMG == rDMG:
-                d1.append(DEF)
-            DEF+=1
-        DEF = 0
-
+                d2.append(DEF)
+    d1 = d2
+    d2 = []
     k = len(d1)
-    while k != 1:
-        j = 0
-        p = 0
-        print('請輸入傷害值')
-        rDMG = int(input())
-        for i in range(16,48):
-            while DEF <= ATK*2:
-                tDMG = math.floor(math.floor((ATK-DEF/2)*(PWR/32+1)*1.75+ATK*i/25.6)*0.25)
-                if tDMG == rDMG:
-                    d2.append(DEF)
-                DEF+=1
-            DEF = 0
-        while j < len(d1):
-            if d1[j] in d2 :
-                d3.append(d1[j])
-            j+=1
-        d1 = d3
-        d2 = []
-        d3 = []
-        k = len(d1)
-    print('敵方的物防是'+str(d1[0]))
+print('敵方的物防是'+str(d1[0]))
 input()
